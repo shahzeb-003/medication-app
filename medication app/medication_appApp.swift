856bc5b470e06c6eff96d69bae59d0f02f6b966c
shahzeb-003 <shahzeb.a956@gmail.com>
2023-11-14 19:28:48 +0000

    Initial Commit

---
+++ medication app/medication_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  medication_appApp.swift
+//  medication app
+//
+//  Created by Shahzeb Ahmad on 14/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct medication_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
